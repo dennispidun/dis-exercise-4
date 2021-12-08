0b61b801cda191cbde84caf426a91d0302c2355f
Vasilis <bountris.vasilis@gmail.com>
2021-12-08 12:48:11 +0100

    Add scripts for eng to spa translation

--- eng-to-spa-translation/base.py
+++ eng-to-spa-translation/base.py
@@ -300,12 +300,3 @@ def build_and_compile_transformer_model():
 
     return transformer
 
-
-# This will have to be commented when running the multi-worker
-
-batch_size = 64
-
-single_worker_dataset = eng_to_span_dataset(batch_size)
-single_worker_model = build_and_compile_transformer_model()
-# single_worker_model.fit(single_worker_dataset, epochs=3, steps_per_epoch=70)
-single_worker_model.fit(single_worker_dataset, epochs=3, )
---
+++ eng-to-spa-translation/multi-worker-training.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+import tensorflow as tf
+
+import base
+
+per_worker_batch_size = 32
+tf_config = json.loads(os.environ["TF_CONFIG"])
+num_workers = len(tf_config["cluster"]["worker"])
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+
+global_batch_size = per_worker_batch_size * num_workers
+multi_worker_dataset = base.eng_to_span_dataset(global_batch_size)
+
+with strategy.scope():
+    # Model building/compiling need to be within `strategy.scope()`.
+    multi_worker_model = base.build_and_compile_transformer_model()
+
+
+multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
