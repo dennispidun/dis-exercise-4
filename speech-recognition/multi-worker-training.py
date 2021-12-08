75c29635893d946d1a2fd392ab5413d9cafac523
Vasilis <bountris.vasilis@gmail.com>
2021-12-08 12:48:36 +0100

    Correct function calls to base script

--- speech-recognition/multi-worker-training.py
+++ speech-recognition/multi-worker-training.py
@@ -12,11 +12,11 @@ num_workers = len(tf_config["cluster"]["worker"])
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = base.ctc_dataset(global_batch_size)
+multi_worker_dataset, num_to_char, char_to_num = base.ctc_dataset(global_batch_size)
 
 with strategy.scope():
     # Model building/compiling need to be within `strategy.scope()`.
-    multi_worker_model = base.build_and_compile_ctc_model()
+    multi_worker_model = base.build_and_compile_ctc_model(num_to_char, char_to_num)
 
 
 multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
