82a790d0ec05282880fb72f1f71a5d58b64a57b1
Vasilis <bountris.vasilis@gmail.com>
2021-12-08 12:07:51 +0100

    Correct batch size and function calls

--- speech-recognition/multi-worker-training.py
+++ speech-recognition/multi-worker-training.py
@@ -5,18 +5,18 @@ import tensorflow as tf
 
 import base
 
-per_worker_batch_size = 64
+per_worker_batch_size = 32
 tf_config = json.loads(os.environ["TF_CONFIG"])
 num_workers = len(tf_config["cluster"]["worker"])
 
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = base.mnist_dataset(global_batch_size)
+multi_worker_dataset = base.ctc_dataset(global_batch_size)
 
 with strategy.scope():
     # Model building/compiling need to be within `strategy.scope()`.
-    multi_worker_model = base.build_and_compile_cnn_model()
+    multi_worker_model = base.build_and_compile_ctc_model()
 
 
 multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
