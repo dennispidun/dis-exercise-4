5e28b44e29a5f6e7225e224ccd43ca5ed8c4e240
Vasilis <bountris.vasilis@gmail.com>
2021-12-23 18:23:12 +0200

    Change model to only save weights

--- eng-to-spa-translation/single-working-training.py
+++ eng-to-spa-translation/single-working-training.py
@@ -4,7 +4,12 @@ import base
 
 batch_size = 64
 
-single_worker_dataset = eng_to_span_dataset(batch_size)
-single_worker_model = build_and_compile_transformer_model()
+single_worker_dataset = base.eng_to_span_dataset(batch_size)
+single_worker_model = base.build_and_compile_transformer_model()
 # single_worker_model.fit(single_worker_dataset, epochs=3, steps_per_epoch=70)
-single_worker_model.fit(single_worker_dataset, epochs=3, )
+single_worker_model.fit(
+    single_worker_dataset,
+    epochs=10,
+)
+
+single_worker_model.save_weights("weights")
